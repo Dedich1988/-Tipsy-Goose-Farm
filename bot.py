3dbc2f9ca955d0c300008f16ff53505e9d54f56e
Dzmitry <dedichpgu@gmail.com>
2023-05-15 11:30:45 +0300

    add rivescript

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from vk_api import VkApi
 from vk_api.longpoll import VkLongPoll, VkEventType
+from rivescript import RiveScript
 
 import os
 from dotenv import load_dotenv
@@ -14,8 +15,25 @@ vk_session = VkApi(token=vk_token)
 # Инициализируем работу с longpoll
 longpoll = VkLongPoll(vk_session)
 
+# Создаем экземпляр RiveScript и загружаем файлы
+bot = RiveScript(utf8=True, debug=False)
+bot.load_directory(".")
+bot.sort_replies()
+
+# Функция для обработки сообщений от пользователя
+def bot_reply(text):
+    reply = bot.reply("user", text)
+    reply_text = reply.strip()
+    return reply_text
+
 # Основной цикл
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-        # Сообщение от пользователя пришло, отправим его обратно
-        vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Hello, world!', 'random_id': 0})
+        # Получаем текст сообщения от пользователя
+        message = event.text
+
+        # Отвечаем, используя RiveScript
+        reply = bot_reply(message)
+
+        # Отправляем ответ пользователю
+        vk_session.method('messages.send', {'user_id': event.user_id, 'message': reply, 'random_id': 0})
